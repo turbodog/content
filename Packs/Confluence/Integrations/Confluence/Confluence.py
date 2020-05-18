26bc456f60819e761501c97e57098bfe15c4cf3a
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-05-18 19:30:29 +0300

    flake

--- Packs/Confluence/Integrations/Confluence/Confluence.py
+++ Packs/Confluence/Integrations/Confluence/Confluence.py
@@ -16,10 +16,7 @@ SERVER = demisto.params()['url'][:-1] if demisto.params()['url'].endswith('/') e
 BASE_URL = SERVER + '/rest/api'
 VERIFY_CERTIFICATE = not demisto.params().get('unsecure', False)
 
-
-
-
-# Support Credentials
+# Support Credentials 1
 USERNAME = demisto.params()['credentials']['identifier']
 PASSWORD = demisto.params()['credentials']['password']
 HEADERS = {
